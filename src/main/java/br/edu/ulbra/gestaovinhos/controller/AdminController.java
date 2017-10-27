70051d7ea15aa9fc1b7d3ec5550bbca5a29cc021
Luan Motta <luanmotta97@hotmail.com>
2017-10-27 12:49:29 -0200

    Add admin/usuario

--- src/main/java/br/edu/ulbra/gestaovinhos/controller/AdminController.java
+++ src/main/java/br/edu/ulbra/gestaovinhos/controller/AdminController.java
@@ -23,9 +23,9 @@ public class AdminController {
     public String inicio(){
         return "inicio";
     }
-    
+
     /*
-     * 
+     *
      * Vinhos
      *
      */
@@ -39,19 +39,56 @@ public class AdminController {
     public String getVinhoById(){
         return "vinho/{id}";
     }
-    
+
     @PostMapping("/vinho/{id}")
     public String updateVinhoById(){
         return "vinho/{id}";
     }
-    
+
     @GetMapping("/vinho/novo")
     public String getNewVinho(){
         return "vinho/novo";
     }
-    
+
     @PostMapping("/vinho/novo")
     public String postNewVinho(){
         return "vinho/novo";
     }
+
+    /*
+     *
+     * Usuario
+     *
+     */
+
+    @GetMapping("/usuario")
+    public String getUsuarios(){
+        return "usuario/listar";
+    }
+
+    @GetMapping("/usuario/{id}")
+    public String getUsuarioById(){
+        return "usuario/{id}";
+    }
+
+    @PostMapping("/usuario/{id}")
+    public String updateUsuarioById(){
+        return "usuario/{id}";
+    }
+
+    @GetMapping("/usuario/novo")
+    public String getNewUsuario(){
+        return "usuario/novo";
+    }
+
+    @PostMapping("/usuario/novo")
+    public String postNewUsuario(){
+        return "usuario/novo";
+    }
+
+    @PostMapping("/usuario/{id}/resetSenha")
+    public String resetSenha(){
+        return "usuario/{id}/resetSenha";
+    }
+
 }
